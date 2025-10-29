55cfa6a9d2b4ec5ab6f8430c8027261c2e09b5e6
mesolano9 <mariaemiliasolano43@gmail.com>
2025-10-29 11:34:31 -0500

    Paquete03 ejercicio

---
+++ Ejemplo01/src/paquete03/ClaseDemo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete03;
+
+/**
+ *
+ * @author USER_ASUS
+ */
+public class ClaseDemo {
+    public static void main(String[] arqs) {
+        System.out.println("Emilia");
+    }
+}
