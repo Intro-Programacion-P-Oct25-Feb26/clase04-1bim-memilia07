c818bd9b8d1b5fdb7da9d41634143b2997e77a3a
mesolano9 <mariaemiliasolano43@gmail.com>
2025-10-30 11:05:20 -0500

    Scanner limpieza

--- Ejemplo02/src/paquete1/Ejemplo2.java
+++ Ejemplo02/src/paquete1/Ejemplo2.java
@@ -31,7 +31,7 @@ public class Ejemplo2 {
         notaAsignatura1 = entrada.nextDouble();
         System.out.println("Ingrese nombre de asignatura 2");
         entrada.nextLine(); // limpieza de buffer
-        nombreAsignatura2 = entrada.nextLine();
+        nombreAsignatura2 = entrada.nextLine(); 
         System.out.println("Ingrese nota de asignatura 2");
         notaAsignatura2 = entrada.nextDouble();
         
